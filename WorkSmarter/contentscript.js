2065ee10cb3e4b69b38f767d8c432feea34a2f02
rustyzone <66030523+rustyzone@users.noreply.github.com>
2022-01-02 19:39:52 +0000

    possible solution

--- WorkSmarter/contentscript.js
+++ WorkSmarter/contentscript.js
@@ -1,10 +1,27 @@
-if (typeof init === 'undefined') {
-    const init = function() {
-        const injectElement = document.createElement('div');
-        injectElement.className = 'test-element';
-        injectElement.innerHTML = 'THIS IS A TEST!!!!';
-        document.body.appendChild(injectElement);
-        console.log('Changed Content On This Page');
-    }
+var elementExist = document.querySelectorAll(
+  "#long-unique-random-id-for-extension"
+);
+if (elementExist.length !== 0) {
+  console.log("elementExits - stop");
+} else {
+  if (typeof init === "undefined") {
+    const init = function () {
+      // create and append css file
+      const css = document.createElement("link");
+      css.setAttribute("rel", "stylesheet");
+      css.setAttribute("type", "text/css");
+      css.setAttribute("href", chrome.runtime.getURL("contentscript.css"));
+      document.head.appendChild(css);
+      // create and append js file
+      const injectElement = document.createElement("div");
+      injectElement.className = "test-element";
+      injectElement.id = "long-unique-random-id-for-extension";
+      injectElement.innerHTML = "THIS IS A TEST!!!!";
+      document.body.appendChild(injectElement);
+      console.log("Changed Content On This Page");
+    };
     init();
+  } else {
+    console.log("Content Already Changed");
+  }
 }
